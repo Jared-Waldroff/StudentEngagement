9912c0066d75d54768b9ac88c0d115d39800977a
Jared-Waldroff <jared.waldroff@gmail.com>
2024-09-18 23:19:24 -0700

    Added in DAiSEE dataset and building the program to read the data to build the model. This model will be able to detect wether or not a student is engaged.

--- src/DAiSEE-engagement/dataset.py
+++ src/DAiSEE-engagement/dataset.py
@@ -1,15 +1,54 @@
-# dataset.py
-import deeplake
+import os
+import cv2
+import pandas as pd
+from torch.utils.data import Dataset
 
-def load_daisee_datasets():
-    # Load training, validation, and testing datasets
-    train_ds = deeplake.load("hub://activeloop/daisee-train")
-    val_ds = deeplake.load("hub://activeloop/daisee-validation")
-    test_ds = deeplake.load("hub://activeloop/daisee-test")
 
-    # Convert datasets to PyTorch Dataloaders
-    train_loader = train_ds.pytorch(num_workers=0, batch_size=4, shuffle=True)
-    val_loader = val_ds.pytorch(num_workers=0, batch_size=4, shuffle=False)
-    test_loader = test_ds.pytorch(num_workers=0, batch_size=4, shuffle=False)
+class DaiseeDataset(Dataset):
+    def __init__(self, labels_file, root_dir, transform=None):
+        """
+        Args:
+            labels_file (string): Path to the CSV file with annotations.
+            root_dir (string): Directory with all the video data.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.labels = pd.read_csv(labels_file)
+        self.root_dir = root_dir
+        self.transform = transform
 
-    return train_loader, val_loader, test_loader
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        # Get video file path
+        video_name = os.path.join(self.root_dir, self.labels.iloc[idx, 0])
+
+        # Read the video frames
+        video_capture = cv2.VideoCapture(video_name)
+        frames = []
+        success, frame = video_capture.read()
+        while success:
+            frames.append(frame)
+            success, frame = video_capture.read()
+        video_capture.release()
+
+        # Convert frames to a numpy array (or any other pre-processing step)
+        video_frames = self.preprocess_frames(frames)
+
+        # Get labels for the video
+        labels = self.labels.iloc[idx, 1:].to_numpy()
+
+        # Create sample
+        sample = {'video': video_frames, 'labels': labels}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def preprocess_frames(self, frames):
+        # Perform any pre-processing you need here
+        # For example, resizing frames or converting to grayscale
+        processed_frames = [cv2.resize(frame, (224, 224)) for frame in frames]  # Example resize
+        return processed_frames
--- src/DAiSEE-engagement/main.py
+++ src/DAiSEE-engagement/main.py
@@ -3,6 +3,7 @@ from dataset import load_daisee_datasets
 from train import train_model
 from save_model import save_model
 
+
 def main():
     # Load the DAiSEE dataset
     train_loader, val_loader, test_loader = load_daisee_datasets()
@@ -12,6 +13,7 @@ def main():
 
     # Save the model
     save_model(model, path='daisee_model.pth')
+
 
 if __name__ == '__main__':
     main()
